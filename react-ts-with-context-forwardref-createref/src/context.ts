4c4e703726d0acff991932ab962f2671a6c9d0de
TrungProUnit <maihuynhtrung0@gmail.com>
2023-03-07 23:18:31 +0700

    first commit lightdark

--- react-ts-with-context-forwardref-createref/src/context.ts
+++ react-ts-with-context-forwardref-createref/src/context.ts
@@ -3,6 +3,6 @@ import { createContext } from "react";
 // trường hợp context là  giá trị mặc định
 export type ThemeContextType = "light" | "dark";
 
-const ThemeContext = createContext<ThemeContextType>("dark");
+const ThemeContext = createContext<ThemeContextType>("light");
 
 export default ThemeContext;
